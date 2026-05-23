b4be0c6d16728c0e3273ac64f124fbad01e41c49
Lizi <86664792+whooslizi@users.noreply.github.com>
2026-05-23 20:57:48 +0700

    fix: why won't it build

--- packaging/build-appimage.sh
+++ packaging/build-appimage.sh
@@ -18,7 +18,6 @@ echo "Setting up AppDir..."
 rm -rf AppDir
 mkdir -p AppDir/usr/bin
 mkdir -p AppDir/usr/share/applications
-mkdir -p AppDir/usr/share/icons/hicolor/256x256/apps
 
 cp dns-fetching AppDir/usr/bin/
 cp dns-fetching-helper AppDir/usr/bin/
@@ -27,15 +26,24 @@ cp assets/dns-fetching.desktop AppDir/
 
 # use icon if it exists, otherwise create a placeholder
 if [ -f assets/icon.png ]; then
+    mkdir -p AppDir/usr/share/icons/hicolor/256x256/apps
     cp assets/icon.png AppDir/usr/share/icons/hicolor/256x256/apps/dns-fetching.png
     cp assets/icon.png AppDir/dns-fetching.png
+    ICON_FLAG="-i AppDir/dns-fetching.png"
+elif [ -f assets/icon.svg ]; then
+    mkdir -p AppDir/usr/share/icons/hicolor/scalable/apps
+    cp assets/icon.svg AppDir/usr/share/icons/hicolor/scalable/apps/dns-fetching.svg
+    cp assets/icon.svg AppDir/dns-fetching.svg
+    ICON_FLAG="-i AppDir/dns-fetching.svg"
 else
-    echo "Warning: no icon.png found in assets/, using placeholder"
-    # create a simple 1x1 PNG as placeholder
-    printf '\x89PNG\r\n\x1a\n' > AppDir/dns-fetching.png
+    echo "Warning: no icon found in assets/, using placeholder SVG"
+    mkdir -p AppDir/usr/share/icons/hicolor/scalable/apps
+    echo '<svg xmlns="http://www.w3.org/2000/svg" width="256" height="256"></svg>' > AppDir/dns-fetching.svg
+    cp AppDir/dns-fetching.svg AppDir/usr/share/icons/hicolor/scalable/apps/dns-fetching.svg
+    ICON_FLAG="-i AppDir/dns-fetching.svg"
 fi
 
-# download linuxdeploy if not present
+# download LinuxDeploy if not present
 if [ ! -f linuxdeploy-x86_64.AppImage ]; then
     echo "Downloading linuxdeploy..."
     curl -fsSL -o linuxdeploy-x86_64.AppImage \
@@ -49,7 +57,7 @@ echo "Packaging AppImage..."
     --appdir AppDir \
     -e AppDir/usr/bin/dns-fetching \
     -d AppDir/usr/share/applications/dns-fetching.desktop \
-    -i AppDir/usr/share/icons/hicolor/256x256/apps/dns-fetching.png \
+    $ICON_FLAG \
     --output appimage
 
 echo "=== Done! ==="
